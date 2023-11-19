b0260d235e8a31af39c24897ceb43221bd09cd1b
vitalspace <myglobalsounds@gmail.com>
2023-11-18 22:53:56 -0600

    updated

--- main.go
+++ main.go
@@ -12,38 +12,69 @@ import (
 	"fmt"
 )
 
+// func main() {
+
+// 	// Crear un contexto de JavaScript
+// 	context := C.JSGlobalContextCreate(nil)
+// 	defer C.JSGlobalContextRelease(context)
+
+// 	// Crear una cadena JavaScript
+// 	jsCode := C.JSStringCreateWithUTF8CString(C.CString("const some = 'hello world'; some"))
+// 	defer C.JSStringRelease(jsCode)
+
+// 	// Evaluar el código JavaScript en el contexto
+// 	result := C.JSEvaluateScript(context, jsCode, nil, nil, 0, nil)
+// 	defer C.JSValueUnprotect(context, result)
+
+// 	// Verificar si ocurrió un error durante la evaluación
+// 	if bool(C.JSValueIsUndefined(context, result)) {
+// 		fmt.Println("Error evaluating JavaScript")
+// 		return
+// 	}
+// 	jsString := C.JSValueToStringCopy(context, result, nil)
+// 	defer C.JSStringRelease(jsString)
+
+// 	// Obtener la longitud de la cadena
+// 	length := C.JSStringGetLength(jsString)
+
+// 	// Obtener la cadena en formato UTF-8
+// 	var buffer [512]C.char
+// 	C.JSStringGetUTF8CString(jsString, &buffer[0], length+1)
+
+// 	// Convertir el puntero C a una cadena de Go
+// 	goString := C.GoString(&buffer[0])
+
+// 	// Imprimir la cadena
+// 	fmt.Println(goString)
+// }
+
+
 func main() {
-
-	// Crear un contexto de JavaScript
+	// Crear un contexto JavaScript
 	context := C.JSGlobalContextCreate(nil)
-	defer C.JSGlobalContextRelease(context)
 
-	// Crear una cadena JavaScript
-	jsCode := C.JSStringCreateWithUTF8CString(C.CString("const some = 'hello world'; some"))
-	defer C.JSStringRelease(jsCode)
+	// Crear un objeto JavaScript
+	globalObject := C.JSContextGetGlobalObject(context)
 
-	// Evaluar el código JavaScript en el contexto
-	result := C.JSEvaluateScript(context, jsCode, nil, nil, 0, nil)
-	defer C.JSValueUnprotect(context, result)
+	// Crear un script de JavaScript
+	script := "var mensaje = '¡Hello world from JavaScript!'; mensaje;"
+	scriptJS := C.JSStringCreateWithUTF8CString(C.CString(script))
+	defer C.JSStringRelease(scriptJS)
 
-	// Verificar si ocurrió un error durante la evaluación
-	if bool(C.JSValueIsUndefined(context, result)) {
-		fmt.Println("Error evaluating JavaScript")
-		return
-	}
-	jsString := C.JSValueToStringCopy(context, result, nil)
-	defer C.JSStringRelease(jsString)
+	// Evaluar el script
+	result := C.JSEvaluateScript(context, scriptJS, globalObject, nil, 1, nil)
 
-	// Obtener la longitud de la cadena
-	length := C.JSStringGetLength(jsString)
+	// Convertir el resultado a una cadena de Go
+	resultStringJS := C.JSValueToStringCopy(context, result, nil)
+	defer C.JSStringRelease(resultStringJS)
 
-	// Obtener la cadena en formato UTF-8
-	var buffer [512]C.char
-	C.JSStringGetUTF8CString(jsString, &buffer[0], length+1)
+	bufferSize := C.JSStringGetMaximumUTF8CStringSize(resultStringJS)
+	resultCString := make([]C.char, bufferSize)
+	C.JSStringGetUTF8CString(resultStringJS, &resultCString[0], bufferSize)
 
-	// Convertir el puntero C a una cadena de Go
-	goString := C.GoString(&buffer[0])
+	// Imprimir el resultado
+	fmt.Printf("Resultado: %s\n", C.GoString(&resultCString[0]))
 
-	// Imprimir la cadena
-	fmt.Println(goString)
+	// Liberar recursos
+	C.JSGlobalContextRelease(context)
 }
