a02e3463905a970f7cad8a67d3268a02e91ec8a3
vitalspace <myglobalsounds@gmail.com>
2023-11-18 22:54:19 -0600

    updated

--- main.go
+++ main.go
@@ -12,43 +12,6 @@ import (
 	"fmt"
 )
 
-// func main() {
-
-// 	// Crear un contexto de JavaScript
-// 	context := C.JSGlobalContextCreate(nil)
-// 	defer C.JSGlobalContextRelease(context)
-
-// 	// Crear una cadena JavaScript
-// 	jsCode := C.JSStringCreateWithUTF8CString(C.CString("const some = 'hello world'; some"))
-// 	defer C.JSStringRelease(jsCode)
-
-// 	// Evaluar el código JavaScript en el contexto
-// 	result := C.JSEvaluateScript(context, jsCode, nil, nil, 0, nil)
-// 	defer C.JSValueUnprotect(context, result)
-
-// 	// Verificar si ocurrió un error durante la evaluación
-// 	if bool(C.JSValueIsUndefined(context, result)) {
-// 		fmt.Println("Error evaluating JavaScript")
-// 		return
-// 	}
-// 	jsString := C.JSValueToStringCopy(context, result, nil)
-// 	defer C.JSStringRelease(jsString)
-
-// 	// Obtener la longitud de la cadena
-// 	length := C.JSStringGetLength(jsString)
-
-// 	// Obtener la cadena en formato UTF-8
-// 	var buffer [512]C.char
-// 	C.JSStringGetUTF8CString(jsString, &buffer[0], length+1)
-
-// 	// Convertir el puntero C a una cadena de Go
-// 	goString := C.GoString(&buffer[0])
-
-// 	// Imprimir la cadena
-// 	fmt.Println(goString)
-// }
-
-
 func main() {
 	// Crear un contexto JavaScript
 	context := C.JSGlobalContextCreate(nil)
