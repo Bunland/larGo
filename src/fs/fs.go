e661fa279eb76e7a931b371e8112bcdd16546fd8
jeremiasbots <98851793+jeremiasbots@users.noreply.github.com>
2023-12-23 00:47:42 -0400

    Update fs.go
    Añadir el objeto de fs.readFile() con la opción encoding ('utf8', 'utf-8' y 'base64') además de condicionales que pronto serán actualizados.

--- src/fs/fs.go
+++ src/fs/fs.go
@@ -10,16 +10,16 @@ extern JSValueRef ReadFileF(JSContextRef context, JSObjectRef function, JSObject
 */
 import "C"
 import (
+	"encoding/base64"
+	"fmt"
 	"os"
 	"unsafe"
 )
 
-// TODO: Hacer más métodos y el objeto de fs.readFile()
-
 // ReadFileF hace la función fs.readFile() de JavaScript
 //
 //export ReadFileF
-func ReadFileF(context C.JSContextRef, function C.JSObjectRef, thisObject C.JSObjectRef, argumentCount C.size_t, arguments *C.JSValueRef, exception *C.JSValueRef) C.JSValueRef {
+func ReadFileF(context C.JSContextRef, function C.JSObjectRef, thisObject C.JSObjectRef, argumentCount C.size_t, arguments *C.JSValueRef, exception *C.JSValueRef) (finalValue C.JSValueRef) {
 	if int(argumentCount) < 1 {
 		return C.JSValueMakeUndefined(context)
 	}
@@ -37,7 +37,38 @@ func ReadFileF(context C.JSContextRef, function C.JSObjectRef, thisObject C.JSOb
 	c_string := C.CString(string(file))
 	file_c_string := C.JSStringCreateWithUTF8CString(c_string)
 	C.free(unsafe.Pointer(c_string))
-	return C.JSValueMakeString(context, file_c_string)
+	if C.JSValueIsUndefined(context, argumentSlice[1]) == true {
+		fmt.Println("Funciona el condicional")
+		return C.JSValueMakeUndefined(context)
+	}
+	obj := C.JSValueToObject(context, argumentSlice[1], exception)
+	if C.JSValueIsUndefined(context, obj) == true {
+		fmt.Println("Funciona el condicional")
+		return C.JSValueMakeUndefined(context)
+	}
+	propertyObjectC := C.CString("encoding")
+	propertyObjectJS := C.JSStringCreateWithUTF8CString(propertyObjectC)
+	value := C.JSObjectGetProperty(context, obj, propertyObjectJS, exception)
+	C.free(unsafe.Pointer(propertyObjectC))
+	defer C.JSStringRelease(propertyObjectJS)
+	if C.JSValueIsUndefined(context, value) == true {
+		fmt.Println("Funciona el condicional")
+		return C.JSValueMakeUndefined(context)
+	}
+	encodingPropertyString := C.JSValueToStringCopy(context, value, exception)
+	bufferSizeString := C.JSStringGetMaximumUTF8CStringSize(encodingPropertyString)
+	bufferString := C.malloc(bufferSizeString)
+	C.JSStringGetUTF8CString(encodingPropertyString, (*C.char)(bufferString), bufferSizeString)
+	encoding := C.GoString((*C.char)(bufferString))
+	C.free(unsafe.Pointer(bufferString))
+	finalValue = C.JSValueMakeString(context, file_c_string)
+	switch encoding {
+	case "base64":
+		cString := C.CString(base64.StdEncoding.EncodeToString(file))
+		finalValue = C.JSValueMakeString(context, C.JSStringCreateWithUTF8CString(cString))
+		C.free(unsafe.Pointer(cString))
+	}
+	return
 }
 
 // ReadFile devuelve la función callback de JavaScript en C para la función ReadFile en JavaScript
