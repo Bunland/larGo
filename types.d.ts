d0845c32eab3ad322dc53dd7fa9f682e1eb89dbe
jeremiasbots <98851793+jeremiasbots@users.noreply.github.com>
2024-01-07 20:29:09 -0500

    Update types.d.ts
    Add console.assert() function

--- types.d.ts
+++ types.d.ts
@@ -1,25 +1,30 @@
 declare const require: (module: string) => any;
-declare const exports: any;
 interface ReadFileObject {
     encoding: "utf8" | "utf-8" | "base64" | "hex"
 }
 type ColorType = "red" | "green" | "blue" | "black" | "cyan" | "yellow" | "magenta" | "white"
-type LarObject = { color: (colorType: ColorType, ...data: any[]) => undefined }
-type ConsoleObject = { time: (label?: string) => undefined, timeEnd: (label?: string) => undefined, log: (...data: any[]) => undefined, clear: () => undefined, error: (message: string) => undefined }
+type LarObject = { color: (colorType: ColorType, ...data: any[]) => undefined, version: string }
+type ConsoleObject = { time: (label?: string) => undefined, timeEnd: (label?: string) => undefined, log: (...data: any[]) => undefined, clear: () => undefined, error: (message: string) => undefined, assert: (expression: boolean, ...obj: any[]) => undefined }
+type ModuleObject = { exports: any }
+type ProcessObject = { version: string }
 declare const Lar: LarObject
 declare const console: ConsoleObject
 declare const Add: (a: number, b: number) => number
 declare const Mult: (a: number, b: number) => number
 declare const Div: (a: number, b: number) => number
 declare const print: (...data: any[]) => undefined
+declare const module: ModuleObject
 declare const prompt: (question: string) => string
+declare const process: ProcessObject
 
 declare module 'fs' {
     declare function readFileSync(fileName: string, obj?: ReadFileObject): string
     declare function writeFileSync(fileName: string, content: string): undefined
+    declare function readdirSync(directoryName: string): Array<string>
 }
 
 declare module 'node:fs' {
     declare function readFileSync(fileName: string, obj?: ReadFileObject): string
     declare function writeFileSync(fileName: string, content: string): undefined
+    declare function readdirSync(directoryName: string): Array<string>
 }
