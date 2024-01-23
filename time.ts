d41d1f8f05c4e9530984661dd777c0c83561ad8f
jeremiasbots <98851793+jeremiasbots@users.noreply.github.com>
2024-01-22 22:32:50 -0500

    Create time.ts

---
+++ time.ts
@@ -0,0 +1,13 @@
+console.time();
+var n = 0;
+for (let i = 0; i < 100000; i++) {
+  n += i;
+}
+console.log(n);
+console.timeLog("default", new Error("test"));
+var x = 0;
+for (let i = 0; i < 100000; i++) {
+  x += i;
+}
+console.log(x);
+console.timeEnd();
