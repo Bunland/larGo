c84a355d4e6a17e48587c8cbd2c68456b6095672
jeremiasbots <98851793+jeremiasbots@users.noreply.github.com>
2024-01-22 22:31:15 -0500

    Añadiendo imports de .json y fs.readFile()

--- src/require/require.go
+++ src/require/require.go
@@ -14,6 +14,7 @@ import (
 	"largo/src/modules"
 	"largo/src/utils"
 	"os"
+	"path/filepath"
 	"strings"
 	"unsafe"
 )
@@ -57,11 +58,58 @@ func createCustomFunction(context C.JSContextRef, globalObject C.JSObjectRef, fu
 	C.JSStringRelease(functionString)
 }
 
+func SetDirnameAndFilename(context C.JSContextRef, globalObject C.JSObjectRef, dirname string, filename string) {
+	dirname_str := C.CString(dirname)
+	dirname_js := C.JSStringCreateWithUTF8CString(dirname_str)
+	C.free(unsafe.Pointer(dirname_str))
+	dirnameString := C.CString("__dirname")
+	dirnameStringJS := C.JSStringCreateWithUTF8CString(dirnameString)
+	dirnameValue := C.CString(dirname)
+	dirnameValueJS := C.JSStringCreateWithUTF8CString(dirnameValue)
+	dirnameValueJSString := C.JSValueMakeString(context, dirnameValueJS)
+	C.JSObjectSetProperty(context, globalObject, dirnameStringJS, dirnameValueJSString, C.kJSPropertyAttributeNone, nil)
+	C.free(unsafe.Pointer(dirnameString))
+	C.free(unsafe.Pointer(dirnameValue))
+	C.JSStringRelease(dirnameStringJS)
+	C.JSStringRelease(dirnameValueJS)
+	C.JSStringRelease(dirname_js)
+	filename_str := C.CString(filename)
+	filename_js := C.JSStringCreateWithUTF8CString(filename_str)
+	C.free(unsafe.Pointer(filename_str))
+	filenameString := C.CString("__filename")
+	filenameStringJS := C.JSStringCreateWithUTF8CString(filenameString)
+	filenameValue := C.CString(filename)
+	filenameValueJS := C.JSStringCreateWithUTF8CString(filenameValue)
+	filenameValueJSString := C.JSValueMakeString(context, filenameValueJS)
+	C.JSObjectSetProperty(context, globalObject, filenameStringJS, filenameValueJSString, C.kJSPropertyAttributeNone, nil)
+	C.free(unsafe.Pointer(filenameString))
+	C.free(unsafe.Pointer(filenameValue))
+	C.JSStringRelease(filenameStringJS)
+	C.JSStringRelease(filenameValueJS)
+	C.JSStringRelease(filename_js)
+}
+
 // Require es la implementación de require de JavaScript
 //
 //export RequireF
 func RequireF(context C.JSContextRef, function C.JSObjectRef, thisObject C.JSObjectRef, argumentCount C.size_t, arguments *C.JSValueRef, exception *C.JSValueRef) (finalValue C.JSValueRef) {
 	argumentSlice := (*[1 << 30]C.JSValueRef)(unsafe.Pointer(arguments))[:argumentCount:argumentCount]
+	// dirnameCString := C.CString("__dirname")
+	// dirnameJS := C.JSStringCreateWithUTF8CString(dirnameCString)
+	// C.free(unsafe.Pointer(dirnameCString))
+	// filenameCString := C.CString("__filename")
+	// filenameJS := C.JSStringCreateWithUTF8CString(filenameCString)
+	// C.free(unsafe.Pointer(filenameCString))
+	// dirnameStr := C.JSValueToStringCopy(context, C.JSObjectGetProperty(context, C.JSContextGetGlobalObject(context), dirnameJS, nil), exception)
+	// filenameStr := C.JSValueToStringCopy(context, C.JSObjectGetProperty(context, C.JSContextGetGlobalObject(context), filenameJS, nil), exception)
+	// dirnameSizeStr := C.JSStringGetMaximumUTF8CStringSize(dirnameStr)
+	// filenameSizeStr := C.JSStringGetMaximumUTF8CStringSize(filenameStr)
+	// dirnameOldValueBuffer := C.malloc(dirnameSizeStr)
+	// filenameOldValueBuffer := C.malloc(filenameSizeStr)
+	// C.JSStringGetUTF8CString(dirnameStr, (*C.char)(dirnameOldValueBuffer), dirnameSizeStr)
+	// C.JSStringGetUTF8CString(filenameStr, (*C.char)(filenameOldValueBuffer), filenameSizeStr)
+	// filenameOldValue := C.GoString((*C.char)(filenameOldValueBuffer))
+	// dirnameOldValue := C.GoString((*C.char)(dirnameOldValueBuffer))
 	if len(argumentSlice) <= 0 {
 		return C.JSValueMakeUndefined(context)
 	}
@@ -80,6 +128,7 @@ func RequireF(context C.JSContextRef, function C.JSObjectRef, thisObject C.JSObj
 			createCustomFunction(context, apiObject, "writeFileSync", C.JSObjectCallAsFunctionCallback(fs.WriteFileSync()))
 			createCustomFunction(context, apiObject, "readdirSync", C.JSObjectCallAsFunctionCallback(fs.ReadDirSync()))
 			createCustomFunction(context, apiObject, "mkdirSync", C.JSObjectCallAsFunctionCallback(fs.MkDirSync()))
+			createCustomFunction(context, apiObject, "readFile", C.JSObjectCallAsFunctionCallback(fs.ReadFile()))
 		}
 		finalValue = (C.JSValueRef)(apiObject)
 	} else {
@@ -89,16 +138,27 @@ func RequireF(context C.JSContextRef, function C.JSObjectRef, thisObject C.JSObj
 			if strings.HasSuffix(module, ".js") && !fileExists(module+".ts") && !fileExists(module+".js") {
 				moduleWithoutSuffix, _ := strings.CutSuffix(module, ".js")
 				content = utils.ReadFile(moduleWithoutSuffix + ".ts")
+				module = moduleWithoutSuffix + ".ts"
 				b = true
 			}
 			if !b {
 				content = utils.ReadFile(module)
 			}
+		} else if strings.HasSuffix(module, ".json") {
+			content = utils.ReadFile(module)
+			jsonCString := C.CString(content)
+			jsonString := C.JSStringCreateWithUTF8CString(jsonCString)
+			C.free(unsafe.Pointer(jsonCString))
+			jsonValueWithContent := C.JSValueMakeFromJSONString(context, jsonString)
+			C.JSStringRelease(jsonString)
+			return jsonValueWithContent
 		} else {
 			if fileExists(module + ".js") {
 				content = utils.ReadFile(module + ".js")
+				module = module + ".js"
 			} else if fileExists(module + ".ts") {
 				content = utils.ReadFile(module + ".ts")
+				module = module + ".ts"
 			} else {
 				return C.JSValueMakeUndefined(context)
 			}
@@ -115,6 +175,9 @@ func RequireF(context C.JSContextRef, function C.JSObjectRef, thisObject C.JSObj
 		C.JSObjectSetProperty(context, thisObject, moduleNameR, moduleObject, C.kJSPropertyAttributeNone, nil)
 		contentC := C.CString(content)
 		script := C.JSStringCreateWithUTF8CString(contentC)
+		absolutePath, _ := filepath.Abs(module)
+		dirname := absolutePath[0 : len(absolutePath)-(len(filepath.Base(module))+1)]
+		SetDirnameAndFilename(context, C.JSContextGetGlobalObject(context), dirname, absolutePath)
 		C.JSEvaluateScript(context, script, nil, nil, 0, exception)
 
 		C.JSStringRelease(script)
@@ -129,6 +192,7 @@ func RequireF(context C.JSContextRef, function C.JSObjectRef, thisObject C.JSObj
 		finalValue = C.JSObjectGetProperty(context, moduleObjectScript, exportsNameJSC, nil)
 		C.free(unsafe.Pointer(exportsName))
 		C.JSStringRelease(exportsNameJSC)
+		// SetDirnameAndFilename(context, C.JSContextGetGlobalObject(context), dirnameOldValue, filenameOldValue)
 	}
 
 	return
